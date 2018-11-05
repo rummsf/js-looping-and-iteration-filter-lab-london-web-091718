3a16a29ad6ccfcf28145bca8b3155b5ec6a4f11e
Irum <irum@Irums-MacBook-Air.local>
2018-11-05 09:53:18 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Code your solution in this file
+function findMatching(drivers, name){
+  return drivers.filter(driver => driver.toLowerCase() === name.toLowerCase())
+}
+
+function fuzzyMatch(drivers, name){
+  return drivers.filter(driver => driver[0] === name[0])
+}
+
+function matchName(drivers, name){
+  return drivers.filter(driver => driver.name === name)
+}
